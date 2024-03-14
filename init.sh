28440aa55f6e8415b6c487e2af976d18c29700f9
connor <connor@connor.hu>
2024-03-14 09:07:41 +0100

    improved init script

--- init.sh
+++ init.sh
@@ -2,5 +2,16 @@
 
 issue=$1
 
-composer install && vendor/bin/phpunit tests/Issues/${issue}Test.php
+composer install || exit $?
+
+vendor/bin/phpunit tests/Issues/GH${issue}Test.php
+
+if [ $? -gt 0 ]
+then
+    echo "It seems the issue is still relevant: https://github.com/symfony/symfony/issues/${issue}"
+    exit 1
+else
+    echo "The problem seems to be solved. Tested in the latest symfony version. https://github.com/symfony/symfony/issues/${issue}"
+    exit 0
+fi
 
